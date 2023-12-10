613db022d998706be5890e971adac2e0d531a083
Kyrylo Dyachenko <104459315+kirill25460@users.noreply.github.com>
2023-12-10 22:42:28 +0200

    update background.jsx

--- src/shared/components/Background/Background.jsx
+++ src/shared/components/Background/Background.jsx
@@ -1,23 +1,11 @@
-import React, { useState } from 'react';
-import { MainContainer,Wrap } from './BackGround.styled';
-import group from '../../images/stains.png';
+import React from 'react';
+import { MainContainer} from './BackGround.styled';
 
 const Background = () => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  const handleMouseMove = e => {
-    setMousePosition({ x: e.clientX, y: e.clientY });
  
-  };
   return (
-    <MainContainer  onMouseMove={handleMouseMove}>
-      <Wrap 
-        style={{ 
-          backgroundImage: `url(${group})`,
-          transform: `translate(-${mousePosition.x / 50}px, 
-          -${mousePosition.y / 50}px)`,
-        }}
-      ></Wrap>
+    <MainContainer>
+     
     </MainContainer>
   );
 };
