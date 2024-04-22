960f8ec478ec999f10197c288a9ece2f20988150
Mazhit <kajsar02@gmail.com>
2024-04-22 22:57:12 +0300

    mooooooon🐮🌕

--- src/components/animationMoon/styled.js
+++ src/components/animationMoon/styled.js
@@ -71,7 +71,7 @@ export const Circle = styled.div`
     animation: ${rotateAnimation} 20s linear infinite;
 
     position: absolute;
-    top: 115%;
+    top: 120%;
     @media (min-width: 489px) {
         top: 100%;
     }
