26fb8283d7f870a5a65cb7d82bad76071a2d2a95
Vlad Melnik <vmelniki2828@gmail.com>
2024-06-20 17:53:16 +0300

    edit modal

--- src/components/productList/modalProduct/modalProduct.jsx
+++ src/components/productList/modalProduct/modalProduct.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import {ModalBackground,ModalContainer,ModalImg,ModalText,CloseIcon,ModalDescription} from "./modalProduct.styled";
-
+import React, { useEffect } from 'react';
+import {
+  ModalBackground,
+  ModalContainer,
+  ModalImg,
+  ModalText,
+  CloseIcon,
+  ModalDescription,
+} from './modalProduct.styled';
 
 const Modal = ({ showModal, closeModal, data }) => {
-    if (!showModal) return null;
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.keyCode === 27) {
+        // закрытие модального окна по клавише "Escape"
+        closeModal();
+      }
+    };
+
+    if (showModal) {
+      document.body.style.overflow = 'hidden'; // блокировка прокрутки при открытии модального окна
+      document.addEventListener('keydown', handleKeyDown);
+    } else {
+      document.body.style.overflow = ''; // разблокировка прокрутки при закрытии модального окна
+      document.removeEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      document.body.style.overflow = ''; // разблокировка прокрутки при размонтировании компонента
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [showModal, closeModal]);
+  if (!showModal) return null;
 
   return (
     <ModalBackground onClick={closeModal}>
       <ModalContainer onClick={e => e.stopPropagation()}>
-        <CloseIcon onClick={closeModal}/>
+        <CloseIcon onClick={closeModal} />
         <ModalText>{data.title}</ModalText>
         <ModalImg src={data.picture}></ModalImg>
         <ModalDescription>{data.description}</ModalDescription>
--- src/components/productList/modalProduct/modalProduct.styled.js
+++ src/components/productList/modalProduct/modalProduct.styled.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
-import { IoClose } from "react-icons/io5";
-
+import { IoClose } from 'react-icons/io5';
 
 export const ModalBackground = styled.div`
   position: fixed;
@@ -9,66 +8,61 @@ export const ModalBackground = styled.div`
   right: 0;
   bottom: 0;
   padding: 45px 20px 50px 60px;
-  background-color:#2F2F2FD9;
+  background-color: #2f2f2fd9;
   display: flex;
   align-items: center;
   justify-content: center;
   width: 1240px;
-height: 739px;
-border-radius: 10px;
-border: 1px solid #717171;
-z-index: 1000;
+  height: 739px;
+  border-radius: 10px;
+  border: 1px solid #717171;
+  z-index: 1000;
+  margin: auto;
 `;
 
 export const ModalContainer = styled.div`
   border-radius: 10px;
- height:100%;
- max-width: 1240px;
+  height: 100%;
+  max-width: 1240px;
   position: relative;
 `;
 
-
-
 export const ModalText = styled.h2`
-font-family: 'DM Sans';
-font-size: 46px;
-font-weight: 500;
-line-height: 47.84px;
-letter-spacing: -0.02em;
-text-align: left;
-
+  font-family: 'DM Sans';
+  font-size: 46px;
+  font-weight: 500;
+  line-height: 47.84px;
+  letter-spacing: -0.02em;
+  text-align: left;
 `;
 
 export const ModalImg = styled.img`
-width: 1120px;
-height: 371px;
-border-radius: 10px ;
-margin-top:30px;
-margin-bottom: 40px;
+  width: 1120px;
+  height: 371px;
+  border-radius: 10px;
+  margin-top: 30px;
+  margin-bottom: 40px;
 `;
 
 export const ModalDescription = styled.p`
-font-family: 'DM Sans';
-font-size: 22px;
-font-weight: 500;
-line-height: 22px;
-text-align: left;
-max-width: 1120px;
+  font-family: 'DM Sans';
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 22px;
+  text-align: left;
+  max-width: 1120px;
 `;
 
-
-
-
 export const CloseIcon = styled(IoClose)`
- color:#AA85FF;
+  color: #aa85ff;
 
- position: absolute;
+  position: absolute;
   top: 10px;
   right: 10px;
   border: none;
   padding: 5px;
   border-radius: 50%;
   width: 24px;
-height: 24px;
-background: #555555;
+  height: 24px;
+  background: #555555;
 `;
